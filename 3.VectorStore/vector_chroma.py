0b42aa141743a2c7e39e0c79f8856859b72673c1
Gagan Deep Yadav <yadavgagan61@gmail.com>
2025-08-28 18:22:04 +0530

    chromedb done

--- 3.VectorStore/vector_chroma.py
+++ 3.VectorStore/vector_chroma.py
@@ -45,11 +45,25 @@ result = vector_store.similarity_search(
     k=1
 )
 
-result2 = vector_store.similarity_search_with_score(
-    query='who among these are a bowler', 
-    k=2
-)
+# result2 = vector_store.similarity_search_with_score(
+#     query='who among these are a bowler', 
+#     k=2
+# )
 #will give us the distance between the embedding vectors
 # means jitna jyada km utna aacha
 
-print(result2)
+# result3 = vector_store.similarity_search_with_score(
+#     query="",
+#     filter= {"team":"Chennai Super Kings"}
+# )
+
+# updated_doc1 = Document(
+#     page_content="Virat Kohli, the former captain of Royal Challengers Bangalore (RCB), is renowned for his aggressive leadership and consistent batting performances. He holds the record for the most runs in IPL history, including multiple centuries in a single season. Despite RCB not winning an IPL title under his captaincy, Kohli's passion and fitness set a benchmark for the league. His ability to chase targets and anchor innings has made him one of the most dependable players in T20 cricket.",
+#     metadata={"team": "Royal Challengers Bangalore"}
+# )
+
+# vector_store.update_document(document_id='73a00e4e-ed23-4c5a-851c-77779c96a8c4', document=updated_doc1)
+
+# vector_store.delete(ids=['73a00e4e-ed23-4c5a-851c-77779c96a8c4'])
+# print(result)
+print(data)
